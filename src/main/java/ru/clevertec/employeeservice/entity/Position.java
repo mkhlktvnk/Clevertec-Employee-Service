8226106952747436a168cb04c95997b4e03c782b
mhlktvnk <mikhail.kutovenko@gmail.com>
2023-06-21 20:51:00 +0300

    Add Position

---
+++ src/main/java/ru/clevertec/employeeservice/entity/Position.java
@@ -0,0 +1,37 @@
+package ru.clevertec.employeeservice.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "positions")
+public class Position {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @ManyToMany(mappedBy = "positions", fetch = FetchType.LAZY)
+    private Set<Employee> employees = new HashSet<>();
+}
