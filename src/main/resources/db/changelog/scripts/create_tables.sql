006a268dd79b4ff66c8ad91e122cd46bdfb846d0
mhlktvnk <mikhail.kutovenko@gmail.com>
2023-06-24 09:20:38 +0300

    Refactor schema, add db.changelog-test.yaml and test_data.sql

--- src/main/resources/db/changelog/scripts/create_tables.sql
+++ src/main/resources/db/changelog/scripts/create_tables.sql
@@ -11,46 +11,40 @@ CREATE TABLE employees
     gender             VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE positions
-(
-    id          SERIAL PRIMARY KEY,
-    name        VARCHAR(255) NOT NULL UNIQUE,
-    description VARCHAR(255) NOT NULL
-);
-
-CREATE TABLE employees_positions
-(
-    employee_id INT NOT NULL,
-    position_id INT NOT NULL,
-    FOREIGN KEY (employee_id) REFERENCES employees (id) ON DELETE CASCADE,
-    FOREIGN KEY (position_id) REFERENCES positions (id) ON DELETE CASCADE,
-    PRIMARY KEY (employee_id, position_id)
-);
-
 CREATE TABLE bonuses
 (
+    id           SERIAL PRIMARY KEY,
+    amount       DECIMAL      NOT NULL,
+    description  VARCHAR(255) NOT NULL,
+    payment_date DATE         NOT NULL,
+    employee_id  BIGINT,
+    CONSTRAINT fk_employee_bonus FOREIGN KEY (employee_id) REFERENCES employees (id)
+);
+
+CREATE TABLE leaves
+(
     id          SERIAL PRIMARY KEY,
-    employee_id INT            NOT NULL,
-    amount      NUMERIC(19, 2) NOT NULL,
-    description VARCHAR(255)   NOT NULL,
-    FOREIGN KEY (employee_id) REFERENCES employees (id) ON DELETE CASCADE
+    start_date  DATE NOT NULL,
+    end_date    DATE NOT NULL,
+    employee_id BIGINT,
+    CONSTRAINT fk_employee_leave FOREIGN KEY (employee_id) REFERENCES employees (id)
 );
 
 CREATE TABLE salaries
 (
     id             SERIAL PRIMARY KEY,
-    amount         NUMERIC(19, 2) NOT NULL,
-    start_date     DATE           NOT NULL,
+    amount         DECIMAL NOT NULL,
+    start_date     DATE    NOT NULL,
     end_date       DATE,
-    current_salary BOOLEAN        NOT NULL,
-    employee_id    INT            NOT NULL,
-    FOREIGN KEY (employee_id) REFERENCES employees (id) ON DELETE CASCADE
+    current_salary BOOLEAN NOT NULL,
+    employee_id    BIGINT,
+    CONSTRAINT fk_employee_salary FOREIGN KEY (employee_id) REFERENCES employees (id)
 );
 
 CREATE TABLE payrolls
 (
     id           SERIAL PRIMARY KEY,
     payment_date DATE NOT NULL,
-    salary_id    INT  NOT NULL,
-    FOREIGN KEY (salary_id) REFERENCES salaries (id) ON DELETE CASCADE
+    salary_id    BIGINT,
+    CONSTRAINT fk_salary_payroll FOREIGN KEY (salary_id) REFERENCES salaries (id)
 );
